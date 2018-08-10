0df85cacc5c402235c8d655630d7b9a4c3c37091
wuxiuran <wuxiuran@bilibili.com>
2018-08-10 15:04:20 +0800

    commit code

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+/**
+ * Created by wuxiuran on 2018/8/10.
+ */
+const path = require('path');
+const config = require('./config')
+
+console.log(process.env.NODE_ENV)
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'production',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: config.build.assetsPublicPath
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  }
+}
